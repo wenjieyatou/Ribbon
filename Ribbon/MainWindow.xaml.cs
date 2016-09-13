7bc5467ea774403ddef6ae428cf94dc93aabfa1c
wenjieyatou <871157174@qq.com>
2016-09-13 21:23:05 +0800

    create a new project

---
+++ Ribbon/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DevExpress.Xpf.Ribbon;
+
+namespace Ribbon
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : DXRibbonWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ggg(object sender, DevExpress.Xpf.Bars.ItemClickEventArgs e)
+        {
+            MessageBox.Show("hajghreauhtgakejrhgkaj");
+        }
+
+
+        private void Limit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void fafa(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("hajghreauhtgakejrhgkaj");
+
+
+
+        }
+
+        private void GYS_Click(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void SHDZ_Click(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void ZYDA_Click(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void BMDA_Click(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void HPDA_Click(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void HPFLClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void DataMaintain_Click(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Limit_click(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
